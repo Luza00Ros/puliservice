64cfcb27eda9f43b4a62eabae08a5f18d590611b
Dino-996 <davidesabia22@gmail.com>
2025-06-10 22:41:22 +0200

    karma installation and configuration

--- src/app/shared/components/alert/alert.component.spec.ts
+++ src/app/shared/components/alert/alert.component.spec.ts
@@ -8,7 +8,7 @@ describe('AlertComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AlertComponent]
+      declarations: [AlertComponent]
     })
     .compileComponents();
 
