21e569fb324c0a86fada384d4b2c4eaa7b9688c8
Dino-996 <davidesabia22@gmail.com>
2025-06-12 00:44:37 +0200

    new featured

---
+++ quote/components/quote/quote.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quote',
+  imports: [],
+  templateUrl: './quote.component.html',
+  styleUrl: './quote.component.css'
+})
+
+export class QuoteComponent {
+
+  protected readonly title = "quote works!";
+
+}
